99b1389e832e0a89e09b599f27415cbbc986aa00
Gagarin-One <krasopeta228@mail.ru>
2022-04-19 11:41:10 +0900

    basic layout

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,34 +1,37 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import s from './Header.module.scss'
 const Header = () => {
   return (
     <div className={s.Wrapper}>
-      <img/>
+      <div className={s.MainLogo}>
+      <img src='/Img/MainLogo.svg'/>
+      </div>
+      
       <div className={s.SectionsWrapper}>
-        <Link to='../HomePage/HomePage.jsx'>
-          <p>Home</p>
-        </Link>
+        <NavLink style={{ textDecoration: 'none' }}  to='../HomePage/HomePage.jsx'>
+          <b>Home</b>
+        </NavLink>
           
-        <Link to='../ShopPage/ShopPage.jsx'>
-          <p>Shop</p>
-        </Link>
+        <NavLink style={{ textDecoration: 'none' }} to='../ShopPage/ShopPage.jsx'>
+          <b>Shop</b>
+        </NavLink>
 
-        <Link to='../WorkshopsPage/Workshop.jsx'>
-          <p>Workshops</p>
-        </Link>
+        <NavLink style={{ textDecoration: 'none' }} to='../WorkshopsPage/Workshop.jsx'>
+          <b>Workshops</b>
+        </NavLink>
 
-        <Link to='../SpacesPage/Spaces.jsx'>
-          <p>Spaces</p>
-        </Link>
+        <NavLink style={{ textDecoration: 'none' }} to='../SpacesPage/Spaces.jsx'>
+          <b>Spaces</b>
+        </NavLink>
           
-        <Link to='../AboutPage/AboutPage.jsx'>
-          <p>About</p>
-        </Link>
+        <NavLink style={{ textDecoration: 'none' }} to='../AboutPage/AboutPage.jsx'>
+          <b>About</b>
+        </NavLink>
         
-        <Link to='../ContactPage/Contact.jsx'>
-          <p>Contact</p>
-        </Link>
+        <NavLink style={{ textDecoration: 'none' }} to='../ContactPage/Contact.jsx'>
+          <b>Contact</b>
+        </NavLink>
       </div>
       <div className={s.SideSectionsWrapper}>
         <img/>
