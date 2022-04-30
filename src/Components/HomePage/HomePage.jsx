49537a8528f40b183829bf314b3d7627c1e9ea7b
Gagarin-One <krasopeta228@mail.ru>
2022-04-30 23:59:01 +0900

    delete footer from home page

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -4,6 +4,7 @@ import Products from '../Products/Products'
 import s from './HomePage.module.scss'
 import {useSelector,useDispatch} from 'react-redux'
 import { getArrayOfProducts } from '../../Redux/Reducer.tsx'
+import Footer from '../footer/Footer'
 const HomePage = () => {
   return(
     <div>
@@ -31,48 +32,7 @@ const HomePage = () => {
       <NavLink to='' style={{ textDecoration: 'none' }}>
         <div>SEE MORE</div>
       </NavLink>
-      
       </div>
-      <div className={s.footer}>
-        <div className={s.footerWrapper}>
-        <NavLink style={{ textDecoration: 'none' }}  to='../HomePage/HomePage.jsx'>
-          <b >Home</b>
-        </NavLink>
-          
-        <NavLink style={{ textDecoration: 'none' }} to='../ShopPage/ShopPage.jsx'>
-          <b>Shop</b>
-        </NavLink>
-
-        <NavLink style={{ textDecoration: 'none' }} to='../WorkshopsPage/Workshop.jsx'>
-          <b>Workshops</b>
-        </NavLink>
-
-        <NavLink style={{ textDecoration: 'none' }} to='../SpacesPage/Spaces.jsx'>
-          <b>Spaces</b>
-        </NavLink>
-          
-        <NavLink style={{ textDecoration: 'none' }} to='../AboutPage/AboutPage.jsx'>
-          <b>About</b>
-        </NavLink>
-        
-        <NavLink style={{ textDecoration: 'none' }} to='../ContactPage/Contact.jsx'>
-          <b>Contact</b>
-        </NavLink>
-        </div >
-        <p className={s.contact}>  PHONE: +45 30 22 82 20 EMAIL: kaja@kajaskytte.dk CVR: 36353481</p>
-        <img src='/Img/Footer_flower.png'/>
-        <div className={s.discount}>
-          <p> GET 10% OFF SIGN UP FOR</p>
-          <p> YOUR NEXT ORDER A NEWSLETTER</p>
-        </div>
-        <div className={s.payItems}>
-          <img className={s.jcb} src='/Img/jcb.png'/>
-          <img className={s.visa} src='Img/visa.png'/>
-          <img src='Img/mobilepay.png'/>
-          <img src='Img/amex.png'/>
-        </div>
-      </div>
-      <img src='/Img/line.png'/>
     </div> 
     </div> 
   )
