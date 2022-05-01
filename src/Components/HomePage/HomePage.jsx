1ce80202bfe60d115e0f3491cd87471a93915e3a
Gagarin-One <krasopeta228@mail.ru>
2022-05-01 16:40:01 +0900

    fixed a layout bug

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import Products from '../Products/Products'
 import s from './HomePage.module.scss'
 import {useSelector,useDispatch} from 'react-redux'
 import { getArrayOfProducts } from '../../Redux/Reducer.tsx'
-import Footer from '../footer/Footer'
+
 const HomePage = () => {
   return(
     <div>
@@ -13,11 +13,21 @@ const HomePage = () => {
         <img src='/Img/FrontImage.svg'/>
       </div>
       <div className={s.NavigationWithPictures}>
-        <img className={s.workshop} src='/Img/Workshops_navigate.svg'/>
-        <img className={s.about} src='/Img/About_navigate.svg'/>
-        <img className={s.contact} src='/Img/Contact_navigate.svg'/>
-        <img className={s.spaces} src='/Img/Spaces_navigate.svg'/>
-        <div className={s.shop}>SHOP</div>
+        <Link to='/'>
+          <img className={s.workshop} src='/Img/1_Image_Navigate.png'/>
+        </Link>
+        <Link to='/'>
+          <img className={s.about} src='/Img/2_Image_Navigate.png'/>
+        </Link>
+        <Link to='/'>
+          <img className={s.contact} src='/Img/3_Image_Navigate.png'/>
+        </Link>
+        <Link to='/'>
+          <img className={s.spaces} src='/Img/4_Image_Navigate.png'/>
+        </Link>
+        <NavLink style={{ textDecoration: 'none' }} className={s.shop} to='/Products'>
+          <div >SHOP</div>
+        </NavLink>
       </div>
       <div className={s.commentWrapper}>
         <div className={s.comment}>
